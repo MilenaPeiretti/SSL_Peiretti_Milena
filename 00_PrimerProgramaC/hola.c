a86c676b819061804660da74af38e8053cec9522
MilenaPeiretti <mpeiretti@frba.utn.edu.ar>
2026-04-06 21:27:44 -0300

    Mi hola mundo

---
+++ 00_PrimerProgramaC/hola.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main() {
+    printf("HOLA MUNDO SOY MILENA PEIRETTI\n");
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"Mi repositorio para la cursada de sintaxis y semantica de los lenguajes"
